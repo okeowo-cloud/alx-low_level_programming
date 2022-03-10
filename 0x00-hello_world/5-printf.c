7b5e8b92941b0276c6523d40a39cf121557aac0b
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-10 11:35:17 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-/*
- * main - prints "exactly with proper grammar,
- * but the outcome is a piece of art,",
- * followed by a new line.
- * Return: 0 if exited properly, non-zero otherwise.
+/**
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
