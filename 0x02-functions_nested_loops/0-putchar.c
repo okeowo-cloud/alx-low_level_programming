05d29fe62f4519dee9f35e67f55a2a49e8532d40
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-15 11:05:29 +0000

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program prints "_putchar"
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+Description of programs:
+
+0-putcahr.c: Program prints "_putchar", followed by a new line.
+
