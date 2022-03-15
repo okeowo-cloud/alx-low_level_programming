e534ca2c58924718a0b439f16800e26e346bb253
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-15 18:00:04 +0000

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - function prints the 9 times table,
+ * starting with 0.
+ *
+ * Return: Always void.
+ */
+void times_table(void)
+{
+	int i, j, r;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (j = 1; j <= 9; j++)
+		{
+			r = (i * j);
+			if ((r / 10) > 0)
+			{
+				_putchar((r / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((r % 10) + '0');
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -18,3 +18,5 @@ Description of programs:
 
 8-24_hours.c: program prints every minutes of the day of Jack Bauer, starting from 00:00 to 23:59.
 
+9-times_table.c: program prints the 9 times table, starting with 0.
+
