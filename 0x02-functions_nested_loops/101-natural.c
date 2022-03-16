b50ce0259b1f369a5399220d0e201dee3f161c51
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-16 11:13:07 +0000

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,17 +10,14 @@ int main(void)
 {
 	int i, sum = 0;
 
-	while (sum < 1024)
+	while (i < 1024)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
 			sum += i;
-			if (sum < 1024)
-			{
-				printf("%d\n", sum);
-			}
 		}
 		i++;
 	}
-	return (sum);
+	printf("%d\n", sum);
+	return (0);
 }
