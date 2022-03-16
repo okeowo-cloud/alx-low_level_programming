120725011487bc36cc2587a0680adfc82f69eb9e
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-16 06:31:32 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,12 +9,12 @@
 int main(void)
 {
 	int i;
+	
+	long t1 = 1;
 
-	int t1 = 1;
+	long t2 = 2;
 
-	int t2 = 2;
-
-	int nextTerm = t1 + t2;
+	long nextTerm = t1 + t2;
 
 	printf("%d, %d, ", t1, t2);
 	for (i = 3; i < 51; i++)
