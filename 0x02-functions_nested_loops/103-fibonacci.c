6ce168c5bd8ab40079934073873f3d82cc13eb20
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-16 07:28:20 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even fibonacci sequence
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	long sum = 0;
+
+	long t1 = 1;
+
+	long t2 = 2;
+
+	long nextTerm = t1 + t2;
+
+	if (t1 % 2 == 0)
+		sum += t1;
+	else if (t2 % 2 == 0)
+		sum += t2;
+	while (sum <= 4000000)
+	{
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+		if (t1 % 2 == 0)
+			sum += t1;
+		else if (t2 % 2 == 0)
+			sum += t2;
+	}
+
+	printf("%ld\n", sum);
+}
