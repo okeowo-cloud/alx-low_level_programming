40880bbc17a19c21ac8ef73d19c1b09364c16606
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-17 03:48:15 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - function checks for uppercase character.
+ * @c: is an int
+ *
+ * Return: 1 if uppercase, 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+Description:
+
+0-isupper.c: program checks for uppercase character. Returns 1 if c is uppercase, 0 otherwise.
+
