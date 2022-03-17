9a1cd212684b7db9d624ed650ff28c5a3216186c
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-17 07:58:23 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100, followed by a new line.
+ * But for multiples of three print Fizz
+ * multiples of five print Buzz
+ * multiples of both three and five print FizzBuzz
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *f = "Fizz", *b = "Buzz", *fb = "FizzBuzz";
+
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i < 100)
+		{
+			if (i % 3 == 0)
+				printf("%s ", f);
+			else if (i % 5 == 0)
+				printf("%s ", b);
+			else if (i % 3 == 0 && i % 5 == 0)
+				printf("%s ", fb);
+			else
+				printf("%d ", i);
+		}
+		else
+		{
+			printf("%s\n", b);
+		}
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -18,3 +18,5 @@ Description:
 
 8-print_square.c: program prints a square, followed by a new line.
 
+9-fizz_buzz.c: prints the numbers from 1 to 100, followed by a new line. But for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.
+
