c5d5710d90aff07a64a37c607c0312b789b47ef6
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-21 16:13:57 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,7 @@ int _atoi(char *s)
 {
 	unsigned int counter = 0, size = 0, oi = 0, om = 1, on = 1, i;
 
-	while (*(s + counter != '\0'))
+	while (*(s + counter) != '\0')
 	{
 		if (size > 0 && (*(s + counter) < '0' || *(s + counter) > '9'))
 			break;
