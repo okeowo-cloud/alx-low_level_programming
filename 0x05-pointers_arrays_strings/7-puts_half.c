7095e7027b370837998cf5bf873c5bf6a76b1ffb
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-21 14:08:54 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,9 +16,9 @@ void puts_half(char *str)
 		length++;
 	}
 	if (length % 2 != 0)
-		n = length / 2;
-	else
 		n = (length + 1) / 2;
+	else
+		n = length / 2;
 	for (i = n; i < length; i++)
 	{
 		_putchar(str[i]);
