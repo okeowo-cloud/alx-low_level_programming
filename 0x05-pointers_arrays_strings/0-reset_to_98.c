7c68be7c0e5d1c2f68a1e024a3167f6772a63474
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-21 03:23:23 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function takes a pointer to an
+ * int as parameter and updates the value it points to to 98.
+ * @n: pointer to an integer.
+ * Return: Always void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+Description:
+
+0-reset_to_98.c: program takes a pointer to an int as parameter and updates the value it points to to 98.
+
