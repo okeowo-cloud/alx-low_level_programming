1929fb533458c44765374dc0afaafada7e5b215e
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-28 13:46:59 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,11 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (*(s + i) != '\0' && *(s + i) != c)
+	while (s[i] != '\0' && s[i] != c)
 		i++;
 
 	if (s[i] == c)
 		return (&s[i]);
 	else
-		return (NULL);
+		return (0);
 }
