ab9f28256ad299a0607264a6c9369387d8f22254
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-28 13:03:07 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,11 +12,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int size = n;
 
-	if (n > 0)
+	if (size > 0)
 	{
 		int i;
 
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 			*(s + i) = b;
 	}
 	return (s);
