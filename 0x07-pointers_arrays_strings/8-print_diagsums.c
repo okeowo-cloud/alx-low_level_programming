e912754df2bf2dd200e48cc8c8fc1ace70010619
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-28 16:17:06 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,23 +11,23 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int sum1, sum2, i, max;
 
-	int counterA = 0, counterB = size - 1;
+	i = sum1 = sum2 = 0;
+	max = size * size;
 
-	int d1 = 0, d2 = 0;
-
-	for (i = 0; i < size; i++)
+	while (i < max)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (j == i + counterA)
-				d1 += (*(a[i] + j));
-
-			if (j == counterB - i)
-				d2 +=  (*(a[i] + j));
-		}
-		counterA++, counterB--;
+		sum1 += a[i];
+		i = i + size + 1;
 	}
-	printf("%d , %d\n", d1, d2);
+
+	i = size - 1;
+	while (i < (max - 1))
+	{
+		sum2 += a[i];
+		i = i + (size - 1);
+	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
