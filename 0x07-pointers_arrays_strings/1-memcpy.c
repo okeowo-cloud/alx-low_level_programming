a648375f9bba7ba251e1b918bac6be02107f76f4
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-28 13:15:53 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - function copies memory area.
+ * @dest: pointer to destination 
+ * @src: pointer to string
+ * @n: is an integer.
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int size = n;
+
+	if (size > 0)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+			*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -2,3 +2,5 @@ Description:
 
 0-memset.c: program fills memory with a constant byte.
 
+1-memcpy.c: program function copies n bytes from memory area src to memory area dest.
+
