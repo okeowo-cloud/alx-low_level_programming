937dcea80c90be67f2dd8b4fce896aa6ae83c18b
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-30 16:39:15 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -29,3 +29,16 @@ int is_palindrome(char *s)
 
 	return (p1(s, len - 1));
 }
+
+/**
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+		return (0);
+	return (1 + _strlen_recursion(++s));
+}
