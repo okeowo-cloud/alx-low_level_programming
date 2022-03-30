b63b164109ee39c57a40f9c141abc4573360d3b1
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-30 14:41:56 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function prints a string in reverse.
+ * @s: is a pointer to string.
+ * Return: is always void.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_ptchar(*s);
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -2,3 +2,5 @@ Description:
 
 0-puts_recursion.c: program prints a string, followed by a new line.
 
+1-print_rev_recursion.c: program prints a string in reverse.
+
