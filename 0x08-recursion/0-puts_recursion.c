6b681185aff1427e63e880b7f5a2288ba62aabd5
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-03-30 14:25:12 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * _puts_recursion - function prints a string, followed by a new line.
+ * @s: is a pointer to string.
+ * Return: is always void.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,4 @@
+Description:
+
+0-puts_recursion.c: program prints a string, followed by a new line.
+
