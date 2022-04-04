fbfb8f9aa0914820e4dc33f367a2e8690352591e
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-04-04 23:15:34 +0100

    0-create_array.c added

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -14,17 +15,19 @@ char *create_array(unsigned int size, char c)
 
     unsigned int i = 0;
 
+    if (size <= 0)
+        return (NULL);
+
     s = malloc(sizeof(char) * size);
 
     if (s == NULL)
-    {
-        exit (1);
-    }
+        return (NULL);
 
     while (i < size)
     {
         s[i] = c;
         i++;
     }
+    s[i] = '\0';
     return (s);
 }
