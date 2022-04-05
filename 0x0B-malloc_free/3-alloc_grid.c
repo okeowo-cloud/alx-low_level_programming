6c608ccad53890bb2cb103bfa7aaee0873b40a6f
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-04-05 00:45:54 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,14 +16,14 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	grid = malloc(sizeof(int *) * height);
+	grid = malloc(height * sizeof(int *));
 
 	if (grid == NULL)
 		return (NULL);
 
 	for (i = 0; i < width; i++)
 	{
-		grid[i] = malloc(sizeof(int) * width);
+		grid[i] = malloc(width * sizeof(int));
 
 		if (grid[i] == NULL)
 		{
