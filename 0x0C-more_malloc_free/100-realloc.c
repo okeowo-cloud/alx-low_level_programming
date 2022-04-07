6b18ccc108c8fe9b32688dff4fccf6694fe729ac
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-04-07 15:51:01 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,9 +13,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *realloc;
 
+	unsigned int i;
+
 	if (old_size == new_size)
 		return (ptr);
-	if (new_size == 0 && ptr != null)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
@@ -30,13 +32,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
-			*((char *) realloc + i) = ptr[i];
+			*((char *) realloc + i) = *((char *) ptr + i);
 		free(ptr);
 	}
 	else
 	{
 		for (i = 0; i < old_size; i++)
-			*((char *) realloc + i) = ptr[i];
+			*((char *) realloc + i) = *((char *) ptr + i);
 		free(ptr);
 	}
 	return (realloc);
