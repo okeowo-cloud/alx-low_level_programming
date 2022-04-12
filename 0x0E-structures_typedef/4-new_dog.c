839a3b03e950fde8c4c6f8a1093f8f90ba969bd7
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-04-12 08:15:56 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -49,7 +49,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
+	{
+		free(new_dog);
 		return (NULL);
+	}
 
 	new_dog->age = age;
 
@@ -77,7 +80,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	else
 		new_dog->owner = NULL;
-	if (new_dog == NULL)
-		return (NULL);
 	return (new_dog);
 }
