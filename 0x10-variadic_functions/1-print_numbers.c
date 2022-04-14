a39833fb0c85ef32b170e7c4ce67f3e8a1d58f0d
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-04-14 08:29:28 -0700

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - function prints numbers, followed by a new line.
+ * @separator: is a pointer to delimiter
+ * @n: is an integer
+ * Return: Always void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+
+	unsigned int i;
+
+	constant char *s = separator;
+
+	while (i < n)
+	{
+		printf("%d", va_arg(args, int));
+		if (i < n - 1 && s != NULL)
+			printf("%s", s);
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -2,3 +2,5 @@ Description:
 
 0-sum_them_all.c: program returns the sum of all its parameters.
 
+1-print_numbers.c: program prints numbers, followed by a new line.
+
