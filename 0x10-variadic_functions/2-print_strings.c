e3be86a1136aa72d06cf11f49a17232ba9a71254
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-04-14 08:59:02 -0700

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdargs.h>
+#include <stdio.h>
+
+/**
+ * print_strings - function prints strings, followed by a new line.
+ * @separator: is a pointer to string
+ * @n: is an int
+ * Return: is always void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+
+	const char *s = separator;
+
+	unsigned int i;
+
+	va_start(args, n);
+
+	i = 0;
+
+	while (i < n)
+	{
+		char *temp = va_arg(args, char *);
+
+		if (temp == NULL)
+			printf("(nil)");
+		else
+			printf("%s", temp);
+
+		if (i < n - 1 && s != NULL)
+			printf("%s", s);
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -4,3 +4,5 @@ Description:
 
 1-print_numbers.c: program prints numbers, followed by a new line.
 
+2-print_strings.c: program prints strings, followed by a new line.
+
