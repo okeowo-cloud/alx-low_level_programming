bf3763b4619b4a4ce59dee905435eb8d16d460a8
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-04-25 01:39:09 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - function prints elements in a linkedlist
+ * @h: is a pointer to a linkedlist
+ * Return: number of element in the list of type size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,4 @@
+Description:
+
+0-print_listint.c: program prints all the elements of a listint_t list.
+
