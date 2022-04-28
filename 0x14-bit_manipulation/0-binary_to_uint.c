16169fef2cd8888e451a1b7cf0ff35be537faead
okeowo-cloud <okeowosamuel58.to@gmail.com>
2022-04-28 18:09:17 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,29 +8,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0, i = 0, tmp, j, power = 1;
-
-    char c;
+    unsigned int result = 0, length = 0, i = 0;
 
     if (b == NULL)
         return (0);
 
-    while (b[i] != '\0')
+    while (b[length])
+        length++;
+    length -= 1;
+
+    while (b[i])
     {
-        c = (const char) b[i];
-        if (!isdigit(c))
+        if (b[i] != '0' || b[i] != '1')
             return (0);
-        tmp = atoi(c);
-        if (tmp != 0 || tmp != 1)
-            return (0);
-        if (i == 0)
-            result += tmp * 1;
-        else
-        {
-            for (j = 0; j < i; j++)
-                power *= 2;
-            result += tmp * power;
-        }
+        if (b[1] == '1')
+            result += (1 * (1 << length));
+        i++;
+        length--;
     }
     return (result);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 unsigned int binary_to_uint(const char *b);
 
