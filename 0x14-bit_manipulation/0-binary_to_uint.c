d8f0c36ec0d78527f898cc9c52bb4d712961ef25
okeowo-cloud <okeowosamuel58.to@gmail.com>
2022-04-28 19:17:55 +0100

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,14 +9,15 @@
 unsigned int binary_to_uint(const char *b)
 {
     unsigned int result = 0, length = 0, i = 0;
-
+    
     if (b == NULL)
         return (0);
-
+    
     while (b[length])
         length++;
+        
     length -= 1;
-
+    
     while (b[i])
     {
         if ((b[i] != '0') && (b[i] != '1'))
