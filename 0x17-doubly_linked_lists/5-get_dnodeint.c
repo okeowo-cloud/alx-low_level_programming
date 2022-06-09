9016fe886802ff55b1f5d7d5cd52478a9ecb6532
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-09 04:40:14 -0700

    5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - functions returns the nth node of
+ * a dlistint_t linked list.
+ *
+ * @head: is a pointer to the head of a dlistint_t list.
+ * @index: is the position of the node in the list.
+ * Return: a pointer to the node, or NULL if not exists.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current;
+
+	unsigned int n = 0;
+
+	current = head;
+
+	if (head == NULL)
+		return (NULL);
+	while (current)
+	{
+		if (n == index)
+			return (current);
+		current = current->next;
+		n++;
+	}
+	return (NULL);
+}
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -2,3 +2,13 @@
 
 0-print_dlistint.c: Program prints all the elements of a (doubly linkedlist) dlistint_t list.
 
+1-dlistint_len.c: Program returns the number of elements in a linked dlistint_t list.
+
+2-add_dnodeint.c: Program adds a new node at the beginning of a dlistint_t list.
+
+3-add_dnodeint_end.c: Program adds a new node at the end of a dlistint_t list.
+
+4-free_dlistint.c: Program frees a dlistint_t list.
+
+5-get_dnodeint.c: Program returns the nth node of a dlistint_t linked list.
+
