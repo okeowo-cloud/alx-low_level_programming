853d39d6d9b27c48f488642a0de366d9c4ee1521
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-06-30 22:13:40 -0700

    0-hash_table_create.c

---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,3 @@
+# Description
+
+0-hash_table_create.c: Program creates a hash table.
---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,41 @@
+#ifndef __HASH_TABLE__
+#define __HASH_TABLE__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * struct hash_node_s - Node of a hash table.
+ *
+ * @key: The key, string
+ * THe key is unique in the hashtable
+ * @value: The value corresponding to a key
+ * @next: a pointer to the next node of a list
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+
+/**
+ * struct hash_table_s - Hash Table data strucuture
+ *
+ * @size: The size of the array
+ * @Array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a LinkedList,
+ * because we watnt our HashTable to use a chaining collision handling.
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif /* __HASH_TABLE__ */
