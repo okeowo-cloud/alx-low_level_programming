fd82536d3c23a85cab98c61d52051181a3736ec1
Tunde Okeowo <okeowosamuel58.to@gmail.com>
2022-08-18 07:06:11 -0700

    mandatory

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
+"""
+Module 5-island_perimeter
+"""
+
+
 def island_perimeter(grid):
+    """
+    function returns the perimeter of the island described in grid
+
+    args:
+    grid - two dimensional integer array of values 0 and 1
+
+    return - integer representing the perimeter of the island described in grid
+    """
     i = 0
     for x in range(len(grid)):
         for y in range(len(grid[0])):
